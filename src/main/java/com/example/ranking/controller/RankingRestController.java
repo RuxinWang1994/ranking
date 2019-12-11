85bca2b5fae75054ceebd8c9a3d5db38b5e731fc
ruxin wang <wrxin1230@gmail.com>
2019-12-11 21:05:50 +0800

    update controller

--- src/main/java/com/example/ranking/controller/RankingRestController.java
+++ src/main/java/com/example/ranking/controller/RankingRestController.java
@@ -1,22 +1,20 @@
 package com.example.ranking.controller;
 
-import java.net.*;
+import static org.springframework.http.HttpStatus.CREATED;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.ranking.dto.employeeDto;
 import com.example.ranking.model.employee;
-import com.example.ranking.model.employees;
 
 @RestController
 @RequestMapping(value = "/employees")
@@ -24,48 +22,68 @@ public class RankingRestController {
 	@Autowired
     private employeeDto employeedto;
 	
-	@GetMapping(path = "/", produces = "application/json")
-	public employees getEmployees() {
-		return employeedto.getAllEmployees();
-		
+	
+	/**
+	 * Delete All
+	 * 
+	 * @return {@link ResponseEntity}
+	 */
+	@DeleteMapping(path = "/erase", produces = "application/json")
+	public ResponseEntity<Void> deleteAll() {
+		employeedto.deleteAll();
+		return ResponseEntity.ok().build();
 	}
 	
-	@GetMapping(path = "/{id}", produces = "application/json")
-	public employee getEmployeeById(Integer id) {
-		return employeedto.getEmployeeById(id);
-	}
 	
-	@PostMapping(path = "/", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<Object> updateEmployee(@RequestBody employee employee){
-		Integer id  = (employeedto.getAllEmployees().getEmployeeMap().size()+1);
-		Integer score = employeedto.getAllEmployees().getById(id).getScore();
-		if (employee.getScore() < score)
-		employee.setScore(score);
-		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(employee.getId())
-				.toUri();
-		return ResponseEntity.created(location).build();
-	}
-	
-	@PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+	/**
+	 * Save
+	 * 
+	 * @return {@link ResponseEntity}
+	 */	
+	@PostMapping(path = "/employee", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<Object> addEmployee(@RequestBody employee employee){
 		Integer id  = (employeedto.getAllEmployees().getEmployeeMap().size()+1);
 		employee.setId(id);
 		employeedto.addEmployee(employee);
 		
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(employee.getId())
-				.toUri();
-		return ResponseEntity.created(location).build();
+		return ResponseEntity.status(CREATED).build();
 	}
 	
-	@DeleteMapping("/erase")
-	public void deleteAllEvents() {
-		employeedto.deleteAll();
+	
+	/**
+	 * Find All
+	 * 
+	 * @return {@link ResponseEntity} List of Events
+	 */
+	@GetMapping(path = "/employees", produces = "application/json")
+	public ResponseEntity<Object> getEmployees() {
+		return ResponseEntity.ok().body(employeedto.getAllEmployees());
+		
 	}
 	
-
+	
+	/**
+	 * Get By Id
+	 * 
+	 * @return {@link ResponseEntity} List of Events
+	 */
+	@GetMapping(path = "/employees/employee/{id}", produces = "application/json")
+	public ResponseEntity<Object> getEmployeeById(@PathVariable("id") Long actorId, Integer id) {
+		return ResponseEntity.ok().body(employeedto.getEmployeeById(id));
+	}
+	
+	
+	/**
+	 * Update
+	 *
+	 * @return {@link ResponseEntity}
+	 */
+	@PutMapping(path = "/employee", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Void> updateEmployee(@RequestBody employee employee){
+		Integer id  = (employeedto.getAllEmployees().getEmployeeMap().size()+1);
+		Integer score = employeedto.getAllEmployees().getById(id).getScore();
+		if (employee.getScore() < score)
+		employee.setScore(score);
+		return ResponseEntity.ok().build();
+	}
 }
